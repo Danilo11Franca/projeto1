f46fcbb520bc60c10f3c8f701e743aa7f0060719
Danilo <danilo11franca@yahoo.com>
2023-07-06 20:18:55 -0300

    Revert "Update funcoes.js"
    This reverts commit 393108505b53e477a109f4eb66d1c02f8fb27600.

---
+++ funcoes.js
@@ -0,0 +1,24 @@
+function soma(x1, x2){
+    return x1 + x2
+}
+
+function multiplicacao(x1, x2){
+    return x1 * x2
+}
+
+function div(x,y) {
+    return x/y
+}
+
+function potencia(x1,x2){
+    return x1**x2
+}
+
+function subtracao(x1,x2){
+    return x1 - x2
+
+}
+
+function raiz(x1){
+    return Math.sqrt(x1)
+}
